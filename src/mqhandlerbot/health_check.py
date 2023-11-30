cb45b0b4616414a0d31a69ca04ae7fe763f3ee70
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-11-30 10:38:25 +0200

    refactor: fix generate amqp connection url

--- src/mqhandlerbot/health_check.py
+++ src/mqhandlerbot/health_check.py
@@ -1,21 +1,15 @@
 import asyncio
-import os
 import sys
 import time
 
 import aio_pika
 from aiormq import AMQPConnectionError
 
+from mqhandlerbot.main import get_amqp_url
+
 
 async def check_connection():
-    address = os.environ.get("AMQP_ADDRESS")
-    port = os.environ.get("AMQP_PORT")
-    user = os.environ.get("AMQP_USER")
-    password = os.environ.get("AMQP_PASSWORD")
-
-    url = f"ampq://{user}:{password}@{address}:{port}/"
-
-    print(url)
+    url = get_amqp_url()
 
     rollback = 1
     while rollback <= 16:
--- src/mqhandlerbot/main.py
+++ src/mqhandlerbot/main.py
@@ -15,12 +15,21 @@ async def run_app() -> None:
     token = os.environ.get("BOT_TOKEN")
     queue = os.environ.get("BOT_QUEUE")
 
-    connection = await aio_pika.connect_robust("")
+    connection = await aio_pika.connect_robust(get_amqp_url())
     publisher = PublisherAdapter(connection, queue)
 
     dp = dispatcher(publisher)
     bot = Bot(token, parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
+
+
+def get_amqp_url() -> str:
+    address = os.environ.get("AMQP_ADDRESS")
+    port = os.environ.get("AMQP_PORT")
+    user = os.environ.get("AMQP_USER")
+    password = os.environ.get("AMQP_PASSWORD")
+
+    return f"ampq://{user}:{password}@{address}:{port}/"
 
 
 if __name__ == "__main__":
